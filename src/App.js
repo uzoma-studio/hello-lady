fb07711c39f9e5412fb48e130c775fb32dd11a52
uzoma <chidumaga@gmail.com>
2022-11-02 10:48:06 +0100

    configures router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import SubmitPage from "./Pages/submit";
+import UploadPage from "./Pages/upload";
+import StartPage from "./Pages/start";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <StartPage />,
+    },
+    {
+      path: "/upload",
+      element: <UploadPage />,
+    },
+    {
+      path: "/submit",
+      element: <SubmitPage />,
+    },
+    {
+      path: "*",
+      element: <StartPage />,
+    },
+  ]);
+
   return (
-    <div className="App">
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
