0b62e3d3a32520b3c6905f4dfd0197c01d200f31
uzoma <chidumaga@gmail.com>
2022-11-03 11:09:27 +0100

    adds photo upload

--- src/Components/agentId.js
+++ src/Components/agentId.js
@@ -1,40 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Styles/Components/agentId.css'
 import badge from '../Images/badge.png'
 
 const AgentId = () => {
-  return (
-    <div className="form-control flex-column">
-        <form className="flex-column">
-            <h3 className="text-center">HELLO LADY AGENT ID</h3>
-            <label for="file" id="file-label" className="text-center">
-                <span style={{fontSize: '14px'}}>+ Upload Photo</span>
-                <input type="file" id="file"/>
-            </label>
-            <ul>
-                <li>
-                    <label for="name">Issued To</label>
-                    <input type="text" id="name" name="name" /> 
-                </li>
-                <li>
-                    <label for="codename">Codename</label>
-                    <input type="text" id="codename" name="codename" /> 
-                </li>
-                <li>
-                    <label for="place-of-issue">Place of Issue</label>
-                    <input type="text" id="place-of-issue" name="place-of-issue" /> 
-                </li>
-            </ul>
-            <div className='flex-row'>
-                <p>
-                    This certifies that the information hereon is that of an appointed HELLO LADY SPECIAL AGENT
-                </p>
-                <img src={badge} alt="Pan-African Rockstar badge" style={{width: '125px'}} />
-            </div>
-        </form>
-        <button className="button-default font-size-s">Get Your ID</button>
-    </div>
-  )
+
+    const [file, setFile] = useState(null)
+
+    const handleFileChange = (event) => {
+        setFile(URL.createObjectURL(event.target.files[0]))
+        console.log(file)
+    }
+
+    return (
+        <div className="form-control flex-column">
+            <form className="flex-column">
+                <h3 className="text-center">HELLO LADY AGENT ID</h3>
+                <label htmlFor="file" id="file-label" className="text-center">
+                    { file ?
+                        <img src={file} alt="hello lady uploaded id" />
+                        :
+                        <>
+                            <span style={{fontSize: '14px'}}>+ Upload Photo</span>
+                            <input type="file" id="file" onChange={(event) => handleFileChange(event)}/>
+                        </>
+                    }
+                </label>
+                <ul>
+                    <li>
+                        <label htmlFor="name">Issued To</label>
+                        <input type="text" id="name" name="name" /> 
+                    </li>
+                    <li>
+                        <label htmlFor="codename">Codename</label>
+                        <input type="text" id="codename" name="codename" /> 
+                    </li>
+                    <li>
+                        <label htmlFor="place-of-issue">Place of Issue</label>
+                        <input type="text" id="place-of-issue" name="place-of-issue" /> 
+                    </li>
+                </ul>
+                <div className='flex-row'>
+                    <p>
+                        This certifies that the information hereon is that of an appointed HELLO LADY SPECIAL AGENT
+                    </p>
+                    <img src={badge} alt="Pan-African Rockstar badge" style={{width: '125px'}} />
+                </div>
+            </form>
+            <button className="button-default font-size-s">Get Your ID</button>
+        </div>
+    )
 }
 
 export default AgentId
