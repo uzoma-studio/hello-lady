d5b37add515f3ba8ed77761ea6bbf942ba329d4c
uzoma <chidumaga@gmail.com>
2022-11-05 22:39:32 +0100

    adds interactive backgrounds

--- src/Components/generateId.js
+++ src/Components/generateId.js
@@ -45,45 +45,68 @@ const AgentId = () => {
         }
     }
 
+    const gradient1 = 'gradient-1'
+    const gradient2 = 'gradient-2'
+    const gradient3 = 'gradient-3'
+    const gradient4 = 'gradient-4'
+    const gradient5 = 'gradient-5'
+
+    const [bgGradient, setBgGradient] = useState(gradient1)
+
+    const setGradient = (event, gradient) => {
+        document.getElementsByClassName('active-gradient')[0].classList.remove('active-gradient')
+        event.target.className += " active-gradient"
+        setBgGradient(gradient)
+    }
+
     return (
         <>
         { !idImage ? 
-            <div className="form-control flex-column">
-                <form id="agent-id" className="flex-column">
-                    <h3 className="text-center">HELLO LADY AGENT ID</h3>
-                    <label htmlFor="file" id="file-label" className="text-center">
-                        { file ?
-                            <img src={file} alt="hello lady uploaded id" />
-                            :
-                            <>
-                                <span style={{fontSize: '14px'}}>+ Upload Photo</span>
-                                <input type="file" id="file" onChange={(event) => handleFileChange(event)}/>
-                            </>
-                        }
-                    </label>
-                    <ul>
-                        <li>
-                            <label htmlFor="name">Issued To</label>
-                            <input type="text" id="name" name="name" ref={nameRef} /> 
-                        </li>
-                        <li>
-                            <label htmlFor="codename">Codename</label>
-                            <input type="text" id="codename" name="codename" ref={codeNameRef} /> 
-                        </li>
-                        <li>
-                            <label htmlFor="place-of-issue">Place of Issue</label>
-                            <input type="text" id="place-of-issue" name="place-of-issue" ref={placeOfIssueRef} /> 
-                        </li>
-                    </ul>
-                    <div className='flex-row'>
-                        <p>
-                            This certifies that the information hereon is that of an appointed HELLO LADY SPECIAL AGENT
-                        </p>
-                        <img src={badge} alt="Pan-African Rockstar badge" style={{width: '125px'}} />
-                    </div>
-                </form>
-                <button className="button-default font-size-s" onClick={() => generateImage()}>Get Your ID</button>
-            </div>
+            <>
+                <ul id="bg-selector">
+                    <li className="gradient-1 active-gradient" onClick={ (e) => setGradient(e, gradient1) }></li>
+                    <li className="gradient-2" onClick={ (e) => setGradient(e, gradient2) }></li>
+                    <li className="gradient-3" onClick={ (e) => setGradient(e, gradient3) }></li>
+                    <li className="gradient-4" onClick={ (e) => setGradient(e, gradient4) }></li>
+                    <li className="gradient-5" onClick={ (e) => setGradient(e, gradient5) }></li>
+                </ul>
+                <div className="form-control flex-column">
+                    <form id="agent-id" className={`flex-column ${bgGradient}`}>
+                        <h3 className="text-center">HELLO LADY AGENT ID</h3>
+                        <label htmlFor="file" id="file-label" className="text-center">
+                            { file ?
+                                <img src={file} alt="hello lady uploaded id" />
+                                :
+                                <>
+                                    <span style={{fontSize: '14px'}}>+ Upload Photo</span>
+                                    <input type="file" id="file" onChange={(event) => handleFileChange(event)}/>
+                                </>
+                            }
+                        </label>
+                        <ul>
+                            <li>
+                                <label htmlFor="name">Issued To</label>
+                                <input type="text" id="name" name="name" ref={nameRef} /> 
+                            </li>
+                            <li>
+                                <label htmlFor="codename">Codename</label>
+                                <input type="text" id="codename" name="codename" ref={codeNameRef} /> 
+                            </li>
+                            <li>
+                                <label htmlFor="place-of-issue">Place of Issue</label>
+                                <input type="text" id="place-of-issue" name="place-of-issue" ref={placeOfIssueRef} /> 
+                            </li>
+                        </ul>
+                        <div className='flex-row'>
+                            <p>
+                                This certifies that the information hereon is that of an appointed HELLO LADY SPECIAL AGENT
+                            </p>
+                            <img src={badge} alt="Pan-African Rockstar badge" style={{width: '125px'}} />
+                        </div>
+                    </form>
+                    <button className="button-default font-size-s" onClick={() => generateImage()}>Get Your ID</button>
+                </div>
+            </>
         :
             <ShareId idImage={idImage} downloadLinkData={downloadLinkData} />
         }
--- src/Pages/start.js
+++ src/Pages/start.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const StartPage = () => {
   return (
-    <main className="page-content">
+    <main className="page-content flex-column space-evenly">
       <h2 className="vcr-text">AN ASSIGNMENT FROM LADY DONLI...</h2>
       <iframe title="An Assignment from Lady Donli" src="https://player.vimeo.com/video/554775452?h=aa2083498c&title=0&byline=0&portrait=0" width="640" height="360" frameBorder="0"></iframe>
       <div className="page-row">
